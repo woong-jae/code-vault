1e40824989c50b4f564b2018b5dc5c53888b778f
woong-jae <jwchung0828@gmail.com>
2023-08-06 01:46:49 +0900

    feat: 프로필 마크업

--- src/app/widgets/Profile/index.tsx
+++ src/app/widgets/Profile/index.tsx
@@ -11,9 +11,11 @@ export default function Profile({
 }) {
   return (
     <Card>
-      <img src={avatar_url} />
-      <div>{login}</div>
-      <div>{name}</div>
+      <div class="flex items-center justify-center">
+        <img class="h-52 w-52 rounded-full" src={avatar_url} />
+      </div>
+      <h2 class="mt-4 text-xl font-bold">{name}</h2>
+      <div class="text-gray-600">{login}</div>
     </Card>
   );
 }
