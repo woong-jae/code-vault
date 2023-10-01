a8f9818f97c7fdcd065ca06b42df68183528db28
woong-jae <jwchung0828@gmail.com>
2023-10-01 17:15:13 +0900

    chore: HMR

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const src = path.join(__dirname, 'src');
@@ -7,44 +6,35 @@ const src = path.join(__dirname, 'src');
 const mode = process.env.NODE_ENV ?? 'development';
 const isProduction = mode === 'production';
 
+const port = 8080;
+
 module.exports = {
   mode,
-  entry: isProduction
-    ? {
-        background: path.join(src, 'app/chrome-extension/background/index.ts'),
-        programmers: path.join(
-          src,
-          'app/chrome-extension/scripts/programmers/index.ts',
-        ),
-        boj: path.join(src, 'app/chrome-extension/scripts/boj/index.ts'),
-        leetcode: path.join(
-          src,
-          'app/chrome-extension/scripts/leetcode/index.ts',
-        ),
-        common_isolated: path.join(
-          src,
-          'app/chrome-extension/scripts/common-isolated/index.ts',
-        ),
-        setting: path.join(src, 'app/setting/index.ts'),
-      }
-    : './src/app/dev.ts',
-  output: isProduction
-    ? {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].js',
-        clean: true,
-      }
-    : {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-      },
+  entry: {
+    background: [path.join(src, 'app/chrome-extension/background/index.ts')],
+    programmers: [
+      path.join(src, 'app/chrome-extension/scripts/programmers/index.ts'),
+    ],
+    boj: [path.join(src, 'app/chrome-extension/scripts/boj/index.ts')],
+    leetcode: [
+      path.join(src, 'app/chrome-extension/scripts/leetcode/index.ts'),
+    ],
+    common_isolated: [
+      path.join(src, 'app/chrome-extension/scripts/common-isolated/index.ts'),
+    ],
+    setting: [path.join(src, 'app/setting/index.ts')],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /(node_modules)/,
         use: {
-          // `.swcrc` can be used to configure swc
           loader: 'swc-loader',
           options: {
             jsc: {
@@ -72,25 +62,18 @@ module.exports = {
       '~': path.resolve(__dirname, 'src'),
     },
   },
-  devtool: isProduction ? false : 'source-map',
+  devtool: isProduction ? false : 'inline-source-map',
   plugins: [
     new CopyPlugin({
       patterns: [{ from: '.', to: '.', context: 'public' }],
       options: {},
     }),
-    ...(isProduction
-      ? []
-      : [new HtmlWebpackPlugin({ template: 'src/dev.html' })]),
   ],
   devServer: {
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-    // client: {
-    //   webSocketURL: 'ws://localhost:8080/ws',
-    // },
-    // devMiddleware: {
-    //   writeToDisk: true,
-    // },
+    port,
+    allowedHosts: 'all',
+    devMiddleware: {
+      writeToDisk: true,
+    },
   },
 };
