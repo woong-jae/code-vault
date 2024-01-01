4850037e31f0b717c3f0a63e532113aa4f861f65
woong-jae <jwchung0828@gmail.com>
2024-01-01 10:55:24 +0900

    저장소 생성 로직 분리

--- src/features/user/ui/CreateRepository.tsx
+++ src/features/user/ui/CreateRepository.tsx
@@ -1,5 +1,4 @@
 import { useMutation } from '@tanstack/react-query';
-import { useState } from 'react';
 import {
   AlertDialogHeader,
   AlertDialogFooter,
@@ -14,6 +13,7 @@ import {
 import { Button } from '@base/components/Button';
 import { Input } from '@base/components/Input';
 import { createRepository } from '..';
+import { useRepositoryName } from '../hooks/repository';
 
 export function CreateRepository({
   accessToken,
@@ -22,7 +22,7 @@ export function CreateRepository({
   accessToken: AccessToken;
   onSuccess: (isSuccess: boolean) => void;
 }) {
-  const [repositoryName, setRepositoryName] = useState('');
+  const { repositoryName, setRepositoryName, isValid } = useRepositoryName();
 
   const createRepositoryMutation = useMutation({
     mutationFn: (repositoryName: string) => {
@@ -38,38 +38,45 @@ export function CreateRepository({
   });
 
   return (
-    <div className="flex items-center space-x-2">
-      <Input
-        id="create-repository"
-        placeholder="저장소 이름을 입력해주세요"
-        value={repositoryName}
-        onChange={({ target: { value } }) => setRepositoryName(value)}
-      />
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button disabled={!repositoryName} className="w-[64px]">
-            생성
-          </Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>정말 생성하시겠습니까?</AlertDialogTitle>
-            <AlertDialogDescription>
-              {'새로운 저장소가 생성되고 "선택한 저장소"로 지정됩니다.'}
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>취소</AlertDialogCancel>
-            <AlertDialogAction
-              onClick={() => {
-                createRepositoryMutation.mutate(repositoryName);
-              }}
-            >
-              확인
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+    <div>
+      <div className="flex items-center space-x-2">
+        <Input
+          id="create-repository"
+          placeholder="저장소 이름을 입력해주세요"
+          value={repositoryName}
+          onChange={({ target: { value } }) => setRepositoryName(value)}
+        />
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button disabled={!repositoryName || !isValid} className="w-[64px]">
+              생성
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>정말 생성하시겠습니까?</AlertDialogTitle>
+              <AlertDialogDescription>
+                {'새로운 저장소가 생성되고 "선택한 저장소"로 지정됩니다.'}
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>취소</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => {
+                  createRepositoryMutation.mutate(repositoryName);
+                }}
+              >
+                확인
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </div>
+      <p className="ml-2 mt-1 text-xs">
+        {
+          '* 저장소 이름에는 ASCII 문자, 숫자, 그리고 ., -, _ 문자만 사용 가능합니다'
+        }
+      </p>
     </div>
   );
 }
